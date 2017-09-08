ab42dd6bb0e08f4160ef87b27bcb3275224f8e8d
jayachandralingam <31743008+jayachandralingam@users.noreply.github.com>
2017-09-08 08:57:49 -0400

    Update dockerdrupal.sh

--- dockerdrupal.sh
+++ dockerdrupal.sh
@@ -1,3 +1,7 @@
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
 sudo yum install -y docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker 
